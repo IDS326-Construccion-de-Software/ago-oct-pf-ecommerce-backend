c1b25e8ed4cf6f5f6e1a58dd3e4c42f932ed1a58
Jaime <jaime251102@gmail.com>
2025-08-29 13:39:25 -0400

    add custom configurarion

---
+++ ago-oct-pf-ecommerce-backend/Configuration/Configuration.cs
@@ -0,0 +1,44 @@
+﻿namespace ago_oct_pf_ecommerce_backend.Configuration
+{
+    public static class Configuration
+    {
+        public static void ConfigureConnection(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            var connectionString = configuration.GetValue<string>("DbSettings:ConnectionString");
+            var databaseName = configuration.GetValue<string>("DbSettings:DatabaseName");
+
+            try
+            {
+                services.AddDbContext<SinewaveDbContext>(options =>
+                options.UseSqlServer(connectionString));
+
+                services.AddScoped<IClosingRepository, ClosingRepository>();
+                services.AddScoped<IConfigurationRepository, ConfigurationRepository>();
+
+
+            }
+            catch (Exception)
+            {
+                /* MONGODB SETTINGS HERE
+                try
+                {
+                    MongoClientConnection clientConnection = new MongoClientConnection(connectionString, databaseName);
+                    services.AddSingleton(clientConnection);
+
+                    services.AddScoped<IApplicationRepository, Mongo.Repositories.ApplicationRepository>();
+                    services.AddScoped<IActionRepository, Mongo.Repositories.ActionRepository>();
+                    services.AddScoped<IRealmRepository, Mongo.Repositories.RealmRepository>();
+                    services.AddScoped<IRealmUserRepository, Mongo.Repositories.RealmUserRepository>();
+                    services.AddScoped<ISubRealmRepository, Mongo.Repositories.SubRealmRepository>();
+                }
+                catch (Exception)
+                {
+                    throw;
+                } */
+            }
+
+
+        }
+    }
+}
