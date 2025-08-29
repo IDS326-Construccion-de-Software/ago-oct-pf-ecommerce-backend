699957d1bf7097a5003dfcfe62b432a70c3ea2fe
Ntgm220 <Noctugamer220@outlook.com>
2025-08-29 19:45:18 -0400

    - Se define el campo para el ConnectionString (Por llenar) - Se define el contexto (incompleto)

--- Revenge.Data/Context/Context.cs
+++ Revenge.Data/Context/Context.cs
@@ -2,11 +2,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace Revenge.Data.Context
 {
-    public class Context
+    public partial class Context : DbContext
     {
-        
+        public Context(DbContextOptions<Context> options)
+            : base(options)
+        {
+        }
+            public virtual DbSet<> Usuarios { get; set; }
     }
 }
