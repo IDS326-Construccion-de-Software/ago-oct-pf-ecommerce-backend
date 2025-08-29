80414b51ac705b01733b918633cb983507ed3f8c
Jaime <jaime251102@gmail.com>
2025-08-29 13:46:48 -0400

    comment example to add dbcontext

--- ago-oct-pf-ecommerce-backend/Configuration/Configuration.cs
+++ ago-oct-pf-ecommerce-backend/Configuration/Configuration.cs
@@ -8,35 +8,12 @@
             var connectionString = configuration.GetValue<string>("DbSettings:ConnectionString");
             var databaseName = configuration.GetValue<string>("DbSettings:DatabaseName");
 
-            try
-            {
-                services.AddDbContext<SinewaveDbContext>(options =>
-                options.UseSqlServer(connectionString));
-
-                services.AddScoped<IClosingRepository, ClosingRepository>();
-                services.AddScoped<IConfigurationRepository, ConfigurationRepository>();
+            //DB context
+            /*services.AddDbContext<SinewaveDbContext>(options =>
+            options.UseSqlServer(connectionString));*/
 
 
-            }
-            catch (Exception)
-            {
-                /* MONGODB SETTINGS HERE
-                try
-                {
-                    MongoClientConnection clientConnection = new MongoClientConnection(connectionString, databaseName);
-                    services.AddSingleton(clientConnection);
 
-                    services.AddScoped<IApplicationRepository, Mongo.Repositories.ApplicationRepository>();
-                    services.AddScoped<IActionRepository, Mongo.Repositories.ActionRepository>();
-                    services.AddScoped<IRealmRepository, Mongo.Repositories.RealmRepository>();
-                    services.AddScoped<IRealmUserRepository, Mongo.Repositories.RealmUserRepository>();
-                    services.AddScoped<ISubRealmRepository, Mongo.Repositories.SubRealmRepository>();
-                }
-                catch (Exception)
-                {
-                    throw;
-                } */
-            }
 
 
         }
