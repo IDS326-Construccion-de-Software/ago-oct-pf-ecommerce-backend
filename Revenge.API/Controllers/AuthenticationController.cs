b662a5fbd08a8c48a72a295e3a962c98ce934c9c
Jaime <jaime251102@gmail.com>
2025-08-30 04:46:52 -0400

    add auth controller (untested)

---
+++ Revenge.API/Controllers/AuthenticationController.cs
@@ -0,0 +1,39 @@
+﻿using System.Runtime.InteropServices;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
+using Revenge.Infrestructure.Repositories;
+
+namespace Revenge.API_oct_pf_ecommerce_backend.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        public readonly IAuthenticationRepository _authenticationRepository;
+
+        public AuthenticationController(IAuthenticationRepository authenticationRepository)
+        {
+            _authenticationRepository = authenticationRepository;
+        }
+
+
+        //endpoint temporal para probar la conexion 
+        [HttpGet]
+        public async Task<bool> Test(CancellationToken cancellationToken)
+        {
+            try
+            {
+                await _authenticationRepository.LoginUserAsync("", "", cancellationToken);
+                return true;
+
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+
+    }
+}
