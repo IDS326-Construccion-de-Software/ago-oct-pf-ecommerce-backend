dd8a69bbc4f75e0adccf66b51b38c80d856da1a5
Jaime <jaime251102@gmail.com>
2025-08-30 04:46:07 -0400

    set up custom configuration

--- Revenge.API/Configuration/Configuration.cs
+++ Revenge.API/Configuration/Configuration.cs
@@ -1,12 +1,23 @@
-﻿namespace ago_oct_pf_ecommerce_backend.Configuration
+﻿using Microsoft.EntityFrameworkCore;
+using Revenge.Data.Context;
+using Revenge.Data.Repositories;
+using Revenge.Infrestructure.Repositories;
+
+namespace ago_oct_pf_ecommerce_backend.Configuration
 {
     public static class Configuration
     {
         public static void ConfigureConnection(this IServiceCollection services,
             IConfiguration configuration)
         {
-            var connectionString = configuration.GetValue<string>("DbSettings:DbConnection");
-            var databaseName = configuration.GetValue<string>("DbSettings:DatabaseName");
+
+            var connectionString = configuration.GetValue<string>("DbConnection");
+
+            AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+
+            services.AddDbContext<RevengeDbContext>(options => options.UseNpgsql(connectionString));
+
+            services.AddScoped<IAuthenticationRepository, AuthenticationRepository>();
         }
     }
 }
