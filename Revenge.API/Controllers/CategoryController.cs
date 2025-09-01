5c68e812f83e08647d0b5528bf48ef5cc98984db
3nguel12 <137580708+3nguel12@users.noreply.github.com>
2025-09-01 17:30:01 -0400

    Creacion del controlador de Categorias en \Controllers, contiene 2 get, 1 put, 1 post y 1 delete

---
+++ Revenge.API/Controllers/CategoryController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Revenge.Data.Context;
+using Revenge.Infrestructure.Entities;
+
+namespace Revenge.API_oct_pf_ecommerce_backend.Controllers
+{
+    [Route("api/category")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly RevengeDbContext _context;
+
+        public CategoryController(RevengeDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/category
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Category>>> GetCategories()
+        {
+            return await _context.Categories.ToListAsync();
+        }
+
+        // GET: api/category/{id}
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Category>> GetCategory(Guid id)
+        {
+            var category = await _context.Categories.FindAsync(id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return category;
+        }
+
+        // PUT: api/category/{id}
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCategory(Guid id, Category category)
+        {
+            if (id != category.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(category).State = EntityState.Modified;
+            category.Updatedat = DateTime.UtcNow;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CategoryExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/category
+        [HttpPost]
+        public async Task<ActionResult<Category>> PostCategory(Category category)
+        {
+            category.Id = Guid.NewGuid();
+            category.Createdat = DateTime.UtcNow;
+
+            _context.Categories.Add(category);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetCategory), new { id = category.Id }, category);
+        }
+
+        // DELETE: api/category/{id}
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCategory(Guid id)
+        {
+            var category = await _context.Categories.FindAsync(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            _context.Categories.Remove(category);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool CategoryExists(Guid id)
+        {
+            return _context.Categories.Any(e => e.Id == id);
+        }
+    }
+}
