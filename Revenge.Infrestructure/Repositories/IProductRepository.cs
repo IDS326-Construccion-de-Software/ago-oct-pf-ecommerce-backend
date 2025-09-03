891ea502cdb6eb3407ddb67b9aaffe8ba95b371b
ANTONIO ORIEL DEL ROSARIO MARTE <1116255@est.intec.edu.do>
2025-09-03 13:18:22 -0400

    added IProductRepository to define Product CRUD operations

---
+++ Revenge.Infrestructure/Repositories/IProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using Revenge.Infrestructure.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Revenge.Infrestructure.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetAllAsync();
+        Task<Product?> GetByIdAsync(Guid id);
+        Task AddAsync(Product product);
+        Task UpdateAsync(Product product);
+        Task DeleteAsync(Guid id);
+        Task<bool> ExistsAsync(Guid id);
+    }
+}
