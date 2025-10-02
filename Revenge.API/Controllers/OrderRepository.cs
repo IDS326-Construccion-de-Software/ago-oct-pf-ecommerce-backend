695a5d134794779cae4c79787a161e40439aa5d6
ANTONIO ORIEL DEL ROSARIO MARTE <1116255@est.intec.edu.do>
2025-10-02 19:44:23 -0400

    Agregados IOrderRepository, OrderRepository y OrdersController para la entidad Order

---
+++ Revenge.API/Controllers/OrderRepository.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Revenge.Data.Context;
+using Revenge.Infrestructure.Entities;
+
+namespace Revenge.API_oct_pf_ecommerce_backend.Controllers
+{
+    [Route("api/order")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        private readonly RevengeDbContext _context;
+
+        public OrdersController(RevengeDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/order
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Order>>> GetOrders()
+        {
+            return await _context.Orders
+                .Include(o => o.User)
+                .Include(o => o.Orderitems)
+                .Include(o => o.Payments)
+                .Include(o => o.Invoices)
+                .ToListAsync();
+        }
+
+        // GET: api/order/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Order>> GetOrder(Guid id)
+        {
+            var order = await _context.Orders
+                .Include(o => o.User)
+                .Include(o => o.Orderitems)
+                .Include(o => o.Payments)
+                .Include(o => o.Invoices)
+                .FirstOrDefaultAsync(o => o.Id == id);
+
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            return order;
+        }
+
+        // PUT: api/order/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutOrder(Guid id, Order order)
+        {
+            if (id != order.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(order).State = EntityState.Modified;
+            order.UpdatedAt = DateTime.UtcNow;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!OrderExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/order
+        [HttpPost]
+        public async Task<ActionResult<Order>> PostOrder(Order order)
+        {
+            order.PlacedAt = DateTime.UtcNow;
+            _context.Orders.Add(order);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetOrder", new { id = order.Id }, order);
+        }
+
+        // DELETE: api/order/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteOrder(Guid id)
+        {
+            var order = await _context.Orders.FindAsync(id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            _context.Orders.Remove(order);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool OrderExists(Guid id)
+        {
+            return _context.Orders.Any(e => e.Id == id);
+        }
+    }
+}
