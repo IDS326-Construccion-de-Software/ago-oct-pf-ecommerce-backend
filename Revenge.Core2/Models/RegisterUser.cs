fe8ba927b849c8dfc57d0479826e4dc4b4243aae
Jaime <jaime251102@gmail.com>
2025-10-18 04:19:59 -0400

    create core2 and add models

---
+++ Revenge.Core2/Models/RegisterUser.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Revenge.Core.Models
+{
+    public class RegisterUserDTO
+    {
+
+        public string Name { get; set; } = null!;
+
+
+        public string Email { get; set; } = null!;
+
+
+        public string Password { get; set; } = null!;
+
+
+        public string? Cellphone { get; set; }
+
+        public DateTime? Birthdate { get; set; }
+
+        public object? Directions { get; set; }
+
+        public int? NumIdentification { get; set; }
+    }
+}
